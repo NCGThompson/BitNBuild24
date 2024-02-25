3d101c795bbb61bea2b88d012ae86fa912392cc4
Nicholas Thompson <NCGThompson@gmail.com>
2024-02-24 23:53:28 -0500

    query user's assigned quadrats

--- back-end/app.py
+++ back-end/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from database import db, User
 
@@ -12,6 +12,21 @@ db.init_app(app)
 def hello_world():
     return "<h3>Hello World</h3><h5>Current User Accounts</h5>\n" + \
         "<br/>".join([str(user) for user in User.query.all()])
+
+@app.route('/my-quadrats')
+def my_quadrats():
+    username = request.args.get('username')
+    if not username:
+        return jsonify({'error': 'Username is required as a query parameter'}), 400
+
+    # Look up the user by the provided username
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    quadrats_data = [{'id': quadrat.id, 'location': quadrat.location} for quadrat in user.quadrats]
+    return jsonify({'quadrats': quadrats_data})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- back-end/database.py
+++ back-end/database.py
@@ -2,10 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+user_quadrat_association = db.Table('user_quadrat',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('quadrat_id', db.Integer, db.ForeignKey('quadrat.id'), primary_key=True)
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    quadrats = db.relationship('Quadrat', secondary=user_quadrat_association, back_populates='users')
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
@@ -14,6 +20,7 @@ class Quadrat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.PickleType, unique=True, nullable=False)
     weight = db.Column(db.Double, unique=False, nullable=True)
+    users = db.relationship('User', secondary=user_quadrat_association, back_populates='quadrats')
 
 class Sample(db.Model):
     id = db.Column(db.Integer, primary_key=True)
